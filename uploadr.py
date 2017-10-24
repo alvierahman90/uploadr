4d392c390119915a9526794dd074835f8ea227e9
alvierahman90 <alvierahman90@users.noreply.github.com>
2017-10-24 21:34:55 +0100

    Working.

---
+++ uploadr.py
@@ -0,0 +1,39 @@
+import time
+import flask
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+port = 8080
+ip =  "192.168.1.80"
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        f =request.files['file']
+        print(secure_filename(f.filename))
+        filename = str(time.time()) + '.' + secure_filename(f.filename).split('.')[-1]
+        f.save('./uploads/' + filename)
+        return render_template('upload_success.html', filename = filename, port = port, ip = ip)
+    else:
+        return render_template('upload.html', port = port, ip = ip)
+
+@app.route('/download/<filename>')
+def download(filename=None):
+    if filename != None:
+        return flask.send_from_directory('uploads', filename, as_attachment=True)
+    else:
+        return hello()
+
+
+@app.route('/css/<filename>')
+def css(filename=None):
+    return flask.send_from_directory('css', filename)
+
+@app.route('/fonts/<filename>')
+def fonts(filename=None):
+    return flask.send_from_directory('fonts', filename)
