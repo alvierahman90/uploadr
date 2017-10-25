12c2dfb4be601248ba380775bc747ad5678b881f
alvierahman90 <alvierahman90@users.noreply.github.com>
2017-10-25 12:50:03 +0100

    Add QR code generator, remove unneccesary javascript from upload_success.html

--- uploadr.py
+++ uploadr.py
@@ -1,11 +1,12 @@
 import time
 import flask
-import config
 import os
+import qrcode
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import send_from_directory
+from config import uploadr as config
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -31,13 +32,33 @@ def upload_file():
                 port = config.port,
                 )
 
+@app.route('/qrcodes/<filename>')
+def qrcodes(filename):
+    link = 'http://{0}:{1}/download/{2}'.format(
+            config.domain
+            , config.port
+            , filename
+            )
+    image = qrcode.make(link)
+    with open('qrcodes/' + filename+ '.png', mode = 'bw+') as file:
+        image.save(file)
+    return send_from_directory('qrcodes', filename + '.png', as_attachment=True)
+
+@app.route('/qr/<filename>')
+def qr(filename=None):
+    return render_template('qr.html', imagesrc = 'http://{0}:{1}/qrcodes/{2}'.format(
+             config.domain
+            , config.port
+            , filename
+            )
+        )
+
 @app.route('/download/<filename>')
 def download(filename=None):
     if filename != None:
         return flask.send_from_directory('uploads', filename, as_attachment=True)
     else:
         return hello()
-
 
 @app.route('/css/<filename>')
 def css(filename=None):
